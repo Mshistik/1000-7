011976ad75a81a341efd3926627cb6877cb2c548
Mshistick <111119238+Mshistik@users.noreply.github.com>
2022-09-27 14:00:24 +0900

    Create 1000-7 C++.cpp

---
+++ 1000-7 C++.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main()
+{
+	for  (int num = 1000; num > 0; num -=7)
+	{
+		std::cout << num << " - 7?" << std::endl;
+		Sleep(30);
+	}
+	std::cout << "I'm bezdar" << std::endl;
+	return 0;
+}
